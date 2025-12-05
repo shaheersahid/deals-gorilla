7c9da7d9df191b8615d862570af541b4ab8ff8d4
khalil ur rehman <needneed20@gmail.com>
2025-12-05 18:52:20 +0500

    Product & Product details page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/product-detail', function () {
+    return view('product-detail');
+});
+Route::get('/product', function () {
+    return view('product');
+});
