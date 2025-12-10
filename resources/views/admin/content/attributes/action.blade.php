6ec90605bebb9f4e968ff7a12a27904c4ca3c617
bilalblaxi11 <83870857+wahabSabir@users.noreply.github.com>
2025-12-10 16:51:28 +0500

    Fix ui issues

--- resources/views/admin/content/attributes/action.blade.php
+++ resources/views/admin/content/attributes/action.blade.php
@@ -1,5 +1,5 @@
 <div class="d-flex gap-2">
-    <a href="{{ route('attributes.edit', $attribute) }}" class="btn btn-sm btn-primary" title="Edit">
+    <a href="{{ route('attributes.edit', $attribute) }}" class="btn btn-sm btn-primary d-inline-flex align-items-center" title="Edit">
         <i class="fa fa-edit"></i>
     </a>
     <form action="{{ route('attributes.destroy', $attribute) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this attribute and all its options?');">
