082f14105d4de2e7f33764efdbe1312c3a045912
shaheershahid <123873702+shaheersahid@users.noreply.github.com>
2025-12-11 17:23:10 +0500

    change footer name

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -2,12 +2,12 @@
     <div class="container-fluid">
         <div class="row align-items-center">
             <div class="col-sm-6">
-                {{ date('Y') }} © Amara Dry Fruit.
+                {{ date('Y') }} © Deals Gorilla.
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
                     Crafted with <i class="mdi mdi-heart text-danger"></i> by <a href="https://codmist.com" target="_blank"
-                        class="text-muted">Codmist Digital Marketing Agency</a>
+                        class="text-muted">Solectronix</a>
                 </div>
             </div>
         </div>
